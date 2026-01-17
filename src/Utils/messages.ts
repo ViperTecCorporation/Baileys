95ad4e9fb1818627b4f324b841880a800d9d7b1a
vini <viniwarrior4@gmail.com>
2026-01-17 20:11:33 -0300

    fix(messages): enhance nullish value checks in message content generation (#2180)

--- src/Utils/messages.ts
+++ src/Utils/messages.ts
@@ -49,6 +49,13 @@ type RequireKey<T, K extends keyof T> = T & {
 
 type WithKey<T, K extends PropertyKey> = T extends unknown ? (K extends keyof T ? RequireKey<T, K> : never) : never
 
+type ExtractByKey<T, K extends PropertyKey> = T extends Record<K, any> ? T : never
+type RequireKey<T, K extends keyof T> = T & {
+	[P in K]-?: Exclude<T[P], null | undefined>
+}
+
+type WithKey<T, K extends PropertyKey> = T extends unknown ? (K extends keyof T ? RequireKey<T, K> : never) : never
+
 type MediaUploadData = {
 	media: WAMediaUpload
 	caption?: string
