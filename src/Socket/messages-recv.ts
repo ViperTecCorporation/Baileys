6e37ff6355ad0f86dbba84e325e8ba6a0bdc9b68
Luiz Braga <btmluiz@outlook.com>
2026-01-20 07:37:34 -0300

    chore: Add messageTimestamp to message updates in messages-recv when receiving a message status update (#2277)

--- src/Socket/messages-recv.ts
+++ src/Socket/messages-recv.ts
@@ -38,6 +38,7 @@ import {
 	MISSING_KEYS_ERROR_TEXT,
 	NACK_REASONS,
 	NO_MESSAGE_FOUND_ERROR_TEXT,
+	toNumber,
 	unixTimestampSeconds,
 	xmppPreKey,
 	xmppSignedPreKey
@@ -1103,7 +1104,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 								'messages.update',
 								ids.map(id => ({
 									key: { ...key, id },
-									update: { status }
+									update: { status, messageTimestamp: toNumber(+(attrs.t ?? 0)) }
 								}))
 							)
 						}
