7e4644b3a777b30c641bc3b4f15d9e4c61cef885
caitano28 <caitano28@gmail.com>
2026-05-06 21:09:53 -0400

    fix: App state sync resilience — skip undecryptable records instead of aborting (#2456)
    (cherry picked from commit 0956f51f9659b083edfca79ea02bc50958f54754)

--- src/Socket/chats.ts
+++ src/Socket/chats.ts
@@ -628,7 +628,8 @@ export const makeChatsSocket = (config: SocketConfig) => {
 									snapshot,
 									getCachedAppStateSyncKey,
 									initialVersionMap[name],
-									appStateMacVerification.snapshot
+									appStateMacVerification.snapshot,
+									logger
 								)
 								states[name] = newState
 								Object.assign(globalMutationMap, mutationMap)
